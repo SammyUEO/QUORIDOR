405e90852a09386c870cd7e86313e4366c670cc8
SammyUEO <146414814+SammyUEO@users.noreply.github.com>
2024-05-14 22:32:41 +0300

    Mouse

---
+++ mouse.js
@@ -0,0 +1,29 @@
+function mousePressed() {
+    
+    // Obține coordonatele absolute ale mouse-ului, în raport cu canvas-ul
+    let absMouseX = mouseX - width / 3.65;
+    let absMouseY = mouseY - height / 3.65;
+  
+    if (currentPlayer === p1 || currentPlayer === p2) {
+      
+        // Calculează indicele celulei pe care a fost făcută clic
+      let i = floor(absMouseX / cellSize);
+      let j = floor(absMouseY / cellSize);
+  
+      // Verifică dacă clicul a fost făcut în interiorul tablei de joc
+      if (i >= 0 && i < cols && j >= 0 && j < rows) {
+        
+        // Verifică dacă celula pe care s-a făcut clic este goală și adiacentă jucătorului curent
+        if ((i === currentPlayer.x && abs(j - currentPlayer.y) === 1) || (j === currentPlayer.y && abs(i - currentPlayer.x) === 1)) {
+          
+            // Mută jucătorul la noua poziție
+          currentPlayer.x = i;
+          currentPlayer.y = j;
+
+          // Schimbă jucătorul curent
+          currentPlayer = (currentPlayer === p1) ? p2 : p1;
+        }
+      }
+    }
+  }
+  
