738d0b3f65388ffd6aa879ae90d9341051047920
SammyUEO <146414814+SammyUEO@users.noreply.github.com>
2024-05-14 22:29:53 +0300

    board

---
+++ Board.js
@@ -0,0 +1,22 @@
+class Board {
+    
+    constructor(bCols, bRows) {
+      this.cols = bCols;
+      this.rows = bRows;
+    }
+  
+    display() {
+      
+        // Desenează celulele
+      for (let i = 0; i < this.cols; i++) {
+        for (let j = 0; j < this.rows; j++) {
+          let x = i * cellSize;
+          let y = j * cellSize;
+          stroke(0);
+          noFill();
+          rect(x, y, cellSize, cellSize);
+        }
+      }
+    }
+  }
+  
