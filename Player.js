906b8c16c758be60efbcaf349d0bbd0a2b579db5
Samuel Pop <samuel.pop@emanuel.ro>
2024-06-05 23:23:59 +0300

    valid move

--- Player.js
+++ Player.js
@@ -1,14 +1,22 @@
 class Player {
-    
-    constructor(px, py, color) {
-      this.x = px;
-      this.y = py;
-      this.color = color;
-    }
-  
-    display() {
-      fill(this.color);
-      noStroke();
-      ellipse(this.x * cellSize + cellSize / 2, this.y * cellSize + cellSize / 2, cellSize * 0.8);
-    }
+  constructor(px, py, color) {
+    this.x = px;
+    this.y = py;
+    this.color = color;
   }
+
+  display() {
+    fill(this.color);
+    noStroke();
+    ellipse(this.x * cellSize + cellSize / 2, this.y * cellSize + cellSize / 2, cellSize * 0.8);
+  }
+
+  validMove(i, j) {
+    return (i === this.x && abs(j - this.y) === 1) || (j === this.y && abs(i - this.x) === 1);
+  }
+
+  move(i, j) {
+    this.x = i;
+    this.y = j;
+  }
+}
