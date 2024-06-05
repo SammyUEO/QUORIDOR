d711553d2ab0f2f87d586980a38762a16e8bee12
Samuel Pop <samuel.pop@emanuel.ro>
2024-06-05 23:22:15 +0300

    tabla

--- Board.js
+++ Board.js
@@ -1,22 +1,18 @@
 class Board {
-    
-    constructor(bCols, bRows) {
-      this.cols = bCols;
-      this.rows = bRows;
-      
-    }
-  
-    display() {
+  constructor(bCols, bRows) {
+    this.cols = bCols;
+    this.rows = bRows;
+  }
 
-        // desenare de celule
-      for (let i = 0; i < this.cols; i++) {
-        for (let j = 0; j < this.rows; j++) {
-          let x = i * cellSize;
-          let y = j * cellSize;
-          stroke(0);
-          noFill();
-          rect(x, y, cellSize, cellSize);
-        }
+  display() {
+    for (let i = 0; i < this.cols; i++) {
+      for (let j = 0; j < this.rows; j++) {
+        let x = i * cellSize;
+        let y = j * cellSize;
+        stroke(0);
+        noFill();
+        rect(x, y, cellSize, cellSize);
       }
     }
   }
+}
