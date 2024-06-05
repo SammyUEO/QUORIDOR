ed82174559eef0fb3d7ed235e4d0bf788c0e0e80
Samuel Pop <samuel.pop@emanuel.ro>
2024-06-05 23:23:43 +0300

    mouse

--- mouse.js
+++ mouse.js
@@ -1,28 +1,28 @@
 function mousePressed() {
-    // Obține coordonatele absolute ale mouse-ului, în raport cu canvas-ul
   let absMouseX = mouseX - width / 3.1;
   let absMouseY = mouseY - height / 4;
-  
-    if (currentPlayer === p1 || currentPlayer === p2) {
-      
-        // Calculează indicele celulei pe care a fost făcută clic
-      let i = floor(absMouseX / cellSize);
-      let j = floor(absMouseY / cellSize);
-  
-      // Verifică dacă clicul a fost făcut în interiorul tablei de joc
-      if (i >= 0 && i < cols && j >= 0 && j < rows) {
-        
-        // Verifică dacă celula pe care s-a făcut clic este goală și folositoare pentru jucătorului curent
-        if ((i === currentPlayer.x && abs(j - currentPlayer.y) === 1) || (j === currentPlayer.y && abs(i - currentPlayer.x) === 1)) {
-          
-            // Mută jucătorul la noua poziție
-          currentPlayer.x = i;
-          currentPlayer.y = j;
 
-          // Schimbă jucătorul curent
-          currentPlayer = (currentPlayer === p1) ? p2 : p1;
-        }
+  
+  
+  if (currentPlayer === p1 || currentPlayer === p2) {
+    let i = floor(absMouseX / cellSize);
+    let j = floor(absMouseY / cellSize);
+  
+    if (i >= 0 && i < cols && j >= 0 && j < rows) {
+      if (currentPlayer.validMove(i, j)) {
+        currentPlayer.move(i, j);
+        currentPlayer = (currentPlayer === p1) ? p2 : p1;
       }
     }
   }
-  
+}
+
+function checkWin() {
+  if (p1.y === rows - 1) {
+    noLoop();
+    alert("Jucătorul turcoaz a câștigat!");
+  } else if (p2.y === 0) {
+    noLoop();
+    alert("Jucătorul bej a câștigat!");
+  }
+}
