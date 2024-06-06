5ce8083db46c9473d2fa0fb7624a0aee22f39171
Samuel Pop <samuel.pop@emanuel.ro>
2024-06-06 03:44:35 +0300

    plasarea de ziduri

--- mouse.js
+++ mouse.js
@@ -2,27 +2,42 @@ function mousePressed() {
   let absMouseX = mouseX - width / 3.1;
   let absMouseY = mouseY - height / 4;
 
-  
-  
   if (currentPlayer === p1 || currentPlayer === p2) {
     let i = floor(absMouseX / cellSize);
     let j = floor(absMouseY / cellSize);
-  
-    if (i >= 0 && i < cols && j >= 0 && j < rows) {
-      if (currentPlayer.validMove(i, j)) {
-        currentPlayer.move(i, j);
-        currentPlayer = (currentPlayer === p1) ? p2 : p1;
+
+    if (placingWall && currentPlayer.wallsLeft > 0) {
+      if (i >= 0 && i < cols && j >= 0 && j < rows) {
+        let wallX = i;
+        let wallY = j;
+        let direction = prompt("Enter direction (h for horizontal, v for vertical):");
+
+        if (direction === 'h' && wallX < cols - 1) {
+          if (board.isWallBetween(wallX, wallY, wallX + 1, wallY) || 
+              board.isWallBetween(wallX + 1, wallY, wallX + 2, wallY)) {
+            return;
+          }
+          let wallColor = currentPlayer.color;
+          board.addWall(wallX, wallY, wallX + 2, wallY, wallColor);
+          currentPlayer.wallsLeft--;
+        } else if (direction === 'v' && wallY < rows - 1) {
+          if (board.isWallBetween(wallX, wallY, wallX, wallY + 1) || 
+              board.isWallBetween(wallX, wallY + 1, wallX, wallY + 2)) {
+            return;
+          }
+          let wallColor = currentPlayer.color;
+          board.addWall(wallX, wallY, wallX, wallY + 2, wallColor);
+          currentPlayer.wallsLeft--;
+        }
+        placingWall = false;
+      }
+    } else {
+      if (i >= 0 && i < cols && j >= 0 && j < rows) {
+        if (currentPlayer.validMove(i, j)) {
+          currentPlayer.move(i, j);
+          currentPlayer = (currentPlayer === p1) ? p2 : p1;
+        }
       }
     }
-  }
-}
-
-function checkWin() {
-  if (p1.y === rows - 1) {
-    noLoop();
-    alert("Jucătorul turcoaz a câștigat!");
-  } else if (p2.y === 0) {
-    noLoop();
-    alert("Jucătorul bej a câștigat!");
   }
 }
