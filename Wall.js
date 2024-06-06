9f34190726337b0b26a18d1971599a031bbfc8ff
Samuel Pop <samuel.pop@emanuel.ro>
2024-06-06 03:41:27 +0300

    aspect

--- Wall.js
+++ Wall.js
@@ -1,15 +1,16 @@
 class Wall {
-    
-    constructor(wx1, wy1, wx2, wy2) {
-      this.x1 = wx1;
-      this.y1 = wy1;
-      this.x2 = wx2;
-      this.y2 = wy2;
-    }
-  
-    display() {
-      stroke(0);
-      line(this.x1 * cellSize, this.y1 * cellSize, this.x2 * cellSize, this.y2 * cellSize);
-    }
+  constructor(wx1, wy1, wx2, wy2, color, thickness) {
+    this.x1 = wx1;
+    this.y1 = wy1;
+    this.x2 = wx2;
+    this.y2 = wy2;
+    this.color = color;
+    this.thickness = 5;
   }
-  
+
+  display() {
+    strokeWeight(this.thickness);
+    stroke(this.color);
+    line(this.x1 * cellSize, this.y1 * cellSize, this.x2 * cellSize, this.y2 * cellSize);
+  }
+}
